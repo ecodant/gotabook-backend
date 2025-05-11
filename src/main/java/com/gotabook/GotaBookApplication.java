d67908901047d98c8585bf69c7bbe7fdf2973336
ecodant <edwincodant@gmail.com>
2025-05-10 22:38:48 -0500

    base project

---
+++ src/main/java/com/gotabook/GotaBookApplication.java
@@ -0,0 +1,13 @@
+package com.gotabook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotaBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GotaBookApplication.class, args);
+	}
+
+}
